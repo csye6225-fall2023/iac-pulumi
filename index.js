9ef3c16b0094d999244082836dc00afd2673a571
Harshil Shah <shah.harshil2@northeastern.edu>
2023-10-10 20:29:13 -0400

    add subnets to vpc

--- index.js
+++ index.js
@@ -13,3 +13,36 @@ const vpc = new aws.ec2.Vpc(vpcConfig.name, {
     },
 });
 
+//Create subnets in your VPC.
+//3 public subnets and 3 private subnets, each in a different availability zone in the same region in the same VPC
+const subnetNames = config.getObject("subnets").names;
+const subnetCidrBlocks = config.getObject("subnets").cidrBlocks;
+const availabilityZones = config.getObject("subnets").availabilityZones;
+const subnetTags = config.getObject("subnets").tags;
+
+const publicSubnets = [];
+const privateSubnets = [];
+
+for (let i = 0; i < 3; i++) {
+    const publicSubnet = new aws.ec2.Subnet(subnetNames.public[i], {
+        vpcId: vpc.id,
+        cidrBlock: subnetCidrBlocks.public[i],
+        availabilityZone: availabilityZones.public[i],
+        mapPublicIpOnLaunch: true,
+        tags: {
+            Name: subnetTags.public[i]
+        }
+    });
+
+    const privateSubnet = new aws.ec2.Subnet(subnetNames.private[i], {
+        vpcId: vpc.id,
+        cidrBlock: subnetCidrBlocks.private[i],
+        availabilityZone: availabilityZones.private[i],
+        tags: {
+            Name: subnetTags.private[i]
+        }
+    });
+
+    publicSubnets.push(publicSubnet);
+    privateSubnets.push(privateSubnet);
+}
