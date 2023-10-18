3f19f9d681e9175e457df0bf6b8230905d81d297
Harshil Shah <shah.harshil2@northeastern.edu>
2023-10-18 10:13:53 -0400

    adding ec2 to infra

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ import * as subnetsModule from "./infrastructure/subnets.js";
 import * as internetGatewayModule from "./infrastructure/internetGateway.js";
 import * as routeTableModule from "./infrastructure/routeTable.js";
 import * as aws from "@pulumi/aws";
+import { createSecurityGroup } from "./infrastructure/securityGroup.js";
+import { createEC2Instance } from "./infrastructure/ec2.js";
 
 const createInfra = (zones) => {
     const vpc = vpcModule.createVpc();
     const { publicSubnets, privateSubnets } = subnetsModule.createSubnets(vpc.id, zones);
     const gw = internetGatewayModule.createInternetGateway(vpc.id);
     routeTableModule.createRouteTablesAndAssociations(vpc.id, gw.id, publicSubnets, privateSubnets);
+    const securityGroup = createSecurityGroup(vpc.id);
+    createEC2Instance(publicSubnets, securityGroup.id);
 }
 
 aws.getRegion({}).then(region => {
