785244b7d0f468a2bc409dd33a366da2171439ae
Harshil Shah <shah.harshil2@northeastern.edu>
2023-10-25 12:00:15 -0400

    unwanted attr

--- infrastructure/parameterGroup.js
+++ infrastructure/parameterGroup.js
@@ -9,7 +9,6 @@ const { family, name} = config.requireObject("rds").parameterGroup;
 export const createParameterGroupRds = () => {
    const parameterGroup =  new aws.rds.ParameterGroup(getResourceName(name), {
         family,
-        name,
         tags: {
             Name: getResourceName(name),
         },
