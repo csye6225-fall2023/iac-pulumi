0a64c8ce74e2c149a777db67302d8ec03de892ce
Harshil Shah <shah.harshil2@northeastern.edu>
2023-11-06 18:27:06 -0500

    attaching iam to ec2

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import * as parameterGroupModule from "./infrastructure/parameterGroup.js";
 import * as securityGroupModule from "./infrastructure/securityGroup.js";
 import * as ec2Module from "./infrastructure/ec2.js";
 import * as rdsModule from "./infrastructure/rds.js";
+import * as iamrolesModule from "./infrastructure/iamroles.js";
 import * as route53Module from "./infrastructure/route53.js";
 
 const createInfra = (zones) => {
@@ -31,8 +32,11 @@ const createInfra = (zones) => {
     // Create RDS
     const database = rdsModule.createRDS(parameterGroupRds.name, RDSSecurityGroup.id, privateSubnets);
 
+    // Create Roles
+    const profile = iamrolesModule.createAndAttachEC2role();
+
     //Create ec2 instance
-    const instance = ec2Module.createEC2Instance(publicSubnets, applicatonSecurityGroup.id, database);
+    const instance = ec2Module.createEC2Instance(publicSubnets, applicatonSecurityGroup.id, database, profile);
 
     //ceate route 53 record
     const route53 = route53Module.createRecord(instance.publicIp);
--- infrastructure/ec2.js
+++ infrastructure/ec2.js
@@ -7,13 +7,14 @@ const ec2 = config.requireObject("ec2");
 const rdsConfig = config.requireObject("rds");
 const { volumeSize, volumeType } = config.requireObject("ec2").rootBlockDevice;
 
-export const createEC2Instance = (publicSubnets, securityGroupId, database) => {
+export const createEC2Instance = (publicSubnets, securityGroupId, database, profile) => {
     const instance = new aws.ec2.Instance(getResourceName(ec2.name), {
         instanceType: ec2.instanceType,
         ami: ec2.amiId, 
         vpcSecurityGroupIds: [securityGroupId],
         subnetId: publicSubnets[0].id,
         keyName: ec2.keyName,
+        iamInstanceProfile: profile.name,
         rootBlockDevice: {
             volumeSize,
             volumeType,
@@ -34,7 +35,9 @@ export const createEC2Instance = (publicSubnets, securityGroupId, database) => {
         echo DB_DIALECT=${rdsConfig.dialect} >> .env
         echo USER_CSV_PATH=./application/users.csv >> .env
         echo NODE_ENV=prod >> .env
-        `
+        sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \        -a fetch-config \    -m ec2 \    -c file:/opt/csye6225/webapp/configs/cloudwatch.config.json \    -s
+        sudo systemctl enable amazon-cloudwatch-agent
+        sudo systemctl start amazon-cloudwatch-agent`
     }, {
         dependsOn: [database]
     });
