d88a858c83a0513fd533c19a778d1a3990cda5c9
Harshil Shah <shah.harshil2@northeastern.edu>
2023-11-14 20:21:33 -0500

    updated route53

--- infrastructure/route53.js
+++ infrastructure/route53.js
@@ -4,13 +4,18 @@ import * as aws from "@pulumi/aws";
 const config = new pulumi.Config();
 const { name, domainName, zoneId, type, ttl } = config.requireObject("dns");
 
-export const createRecord = (instancePublicIp) => {
+export const createRecord = (awsAlb) => {
     const record = new aws.route53.Record(name, {
         zoneId,
         name: domainName,
         type,
-        ttl,
-        records: [instancePublicIp],
+        aliases: [
+            {
+                name: awsAlb.dnsName,
+                zoneId: awsAlb.zoneId,
+                evaluateTargetHealth: true
+            }
+        ]
     });
 
     return record;
