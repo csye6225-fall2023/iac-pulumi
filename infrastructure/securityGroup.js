7dbd33636108aa3a7b570f4948b8690a8cb0326c
Harshil Shah <shah.harshil2@northeastern.edu>
2023-11-14 20:20:04 -0500

    alb and ec2 security groups updated

--- infrastructure/securityGroup.js
+++ infrastructure/securityGroup.js
@@ -3,27 +3,72 @@ import * as aws from "@pulumi/aws";
 import { getResourceName } from "../helper/resourceName.js";
 
 const config = new pulumi.Config();
-const applicationSecurityGroup = config.requireObject("security-group").applicationSecurityGroup;
+const applicationSecurityGroupConfig = config.requireObject("security-group").applicationSecurityGroup;
 const rdsSecurityGroup = config.requireObject("security-group").rdsSecurityGroup;
+const loadBalancerSecurityGroupConfig = config.requireObject("security-group").loadBalancerSecurityGroup;
 
 export const createSecurityGroups = (vpcId) => {
-    const applicatonSecurityGroup = new aws.ec2.SecurityGroup(getResourceName(applicationSecurityGroup.name), {
+    const loadBalancerSecurityGroup = new aws.ec2.SecurityGroup(getResourceName(loadBalancerSecurityGroupConfig.name), {
         vpcId,
-        ingress: applicationSecurityGroup.ingressRules.map(rule => ({
+        ingress: loadBalancerSecurityGroupConfig.ingressRules.map(rule => ({
             protocol: rule.protocol,
             fromPort: rule.fromPort,
             toPort: rule.toPort,
             cidrBlocks: rule.cidrBlocks,
             ipv6CidrBlocks: rule.ipv6CidrBlocks,
         })),
-        egress: applicationSecurityGroup.egressRules.map(rule => ({
+        egress: loadBalancerSecurityGroupConfig.egressRules.map(rule => ({
             protocol: rule.protocol,
             fromPort: rule.fromPort,
             toPort: rule.toPort,
             cidrBlocks: rule.cidrBlocks,
         })),
         tags: {
-            Name: getResourceName(applicationSecurityGroup.name),
+            Name: getResourceName(loadBalancerSecurityGroupConfig.name),
+        },
+    });
+
+    const applicatonSecurityGroup = new aws.ec2.SecurityGroup(getResourceName(applicationSecurityGroupConfig.name), {
+        vpcId,
+        ingress: applicationSecurityGroupConfig.ingressRules.map(rule => ({
+            protocol: rule.protocol,
+            fromPort: rule.fromPort,
+            toPort: rule.toPort,
+            securityGroups: [loadBalancerSecurityGroup.id]
+        })),
+        egress: applicationSecurityGroupConfig.egressRules.map(rule => ({
+            protocol: rule.protocol,
+            fromPort: rule.fromPort,
+            toPort: rule.toPort,
+            cidrBlocks: rule.cidrBlocks,
+        })),
+        // ingress: [
+        //     // Allow TCP traffic on port 22 from the load balancer security group
+        //     { 
+        //         protocol: "tcp", 
+        //         fromPort: 22, 
+        //         toPort: 22, 
+        //         securityGroups: [loadBalancerSecurityGroup.id] 
+        //     },
+        //     // Allow TCP traffic on port 8080 from the load balancer security group
+        //     { 
+        //         protocol: "tcp", 
+        //         fromPort: 8080, 
+        //         toPort: 8080, 
+        //         securityGroups: [loadBalancerSecurityGroup.id] 
+        //     },
+        // ],
+        // egress: [
+        //     {
+        //         protocol: "-1",
+        //         fromPort: 0,
+        //         toPort: 0,
+        //         cidrBlocks: ["0.0.0.0/0"]
+                
+        //     }
+        // ],
+        tags: {
+            Name: getResourceName(applicationSecurityGroupConfig.name),
         },
     });
 
@@ -41,6 +86,6 @@ export const createSecurityGroups = (vpcId) => {
             Name: getResourceName(rdsSecurityGroup.name),
         },
     });
-  
-    return { applicatonSecurityGroup, RDSSecurityGroup };
+ 
+    return { applicatonSecurityGroup, RDSSecurityGroup, loadBalancerSecurityGroup };
 }
