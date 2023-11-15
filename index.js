d1012a1c95c3061fe630debf857f5c6debff63bd
Harshil Shah <shah.harshil2@northeastern.edu>
2023-11-14 20:21:54 -0500

    updated main

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ import * as internetGatewayModule from "./infrastructure/internetGateway.js";
 import * as routeTableModule from "./infrastructure/routeTable.js";
 import * as parameterGroupModule from "./infrastructure/parameterGroup.js";
 import * as securityGroupModule from "./infrastructure/securityGroup.js";
-import * as ec2Module from "./infrastructure/ec2.js";
+import * as ec2TemplateModule from "./infrastructure/ec2LaunchTemplate.js";
 import * as rdsModule from "./infrastructure/rds.js";
 import * as iamrolesModule from "./infrastructure/iamroles.js";
 import * as route53Module from "./infrastructure/route53.js";
+import * as autoScalingGroupModule from "./infrastructure/autoScalingGroup.js";
+import * as loadBalancerModule from "./infrastructure/loadBalancer.js";
 
 const createInfra = (zones) => {
     // Create a vpc
@@ -24,7 +26,7 @@ const createInfra = (zones) => {
     routeTableModule.createRouteTablesAndAssociations(vpc.id, gw.id, publicSubnets, privateSubnets);
 
     // Create security groups
-    const { applicatonSecurityGroup, RDSSecurityGroup } = securityGroupModule.createSecurityGroups(vpc.id); 
+    const { applicatonSecurityGroup, RDSSecurityGroup, loadBalancerSecurityGroup } = securityGroupModule.createSecurityGroups(vpc.id); 
 
     // Create RDS Parameter Group
     const parameterGroupRds = parameterGroupModule.createParameterGroupRds(); 
@@ -35,11 +37,17 @@ const createInfra = (zones) => {
     // Create Roles
     const profile = iamrolesModule.createAndAttachEC2role();
 
-    //Create ec2 instance
-    const instance = ec2Module.createEC2Instance(publicSubnets, applicatonSecurityGroup.id, database, profile);
+    //create load balancer
+    const {loadBalancer, targetGroup} = loadBalancerModule.createLoadBalancer(publicSubnets.map(subnet => subnet.id), loadBalancerSecurityGroup, vpc.id);
+
+    //create launch template
+    const launchTemplate = ec2TemplateModule.createLaunchTemplate(applicatonSecurityGroup.id, database, profile);
+
+    //create autoscaling group
+    const asg = autoScalingGroupModule.createAutoScalingGroup(launchTemplate, publicSubnets.map(subnet => subnet.id), targetGroup);
 
     //ceate route 53 record
-    const route53 = route53Module.createRecord(instance.publicIp);
+    const route53 = route53Module.createRecord(loadBalancer);
 }
 
 aws.getRegion({}).then(region => {
